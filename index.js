fd44c80f0dad982da59ae06910cf8d2476b1d71c
Anatoliy Gatt <anatoliy.gatt@aol.com>
2015-09-23 11:43:23 +0200

    Replace " with ' in definitions of all strings in index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@
  * @license MIT
  */
 
-"use strict";
+'use strict';
 
 /**
  * @public
@@ -15,4 +15,4 @@
  * @returns {Function} - Function to initialize unchangeable instance of Telize.
  */
 
-module.exports = require("./lib/telize");
+module.exports = require('./lib/telize');
