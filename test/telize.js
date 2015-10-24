7c406181e1e50acdf6c559a1f0b8a6bcae7a4b3b
Anatoliy Gatt <anatoliy.gatt@aol.com>
2015-10-24 07:20:59 +0200

    Fix anonymous functions declaration coding style in telize.js tests

--- test/telize.js
+++ test/telize.js
@@ -1,11 +1,11 @@
 var assert = require('assert');
 var telize = require('../lib/telize')();
 
-describe('telize', function() {
-    describe('init', function() {
+describe('telize', function () {
+    describe('init', function () {
         var defaultRequestOptions = {};
 
-        before(function() {
+        before(function () {
             defaultRequestOptions = {
                 hostname: 'www.telize.com',
                 port: 80,
@@ -13,11 +13,11 @@ describe('telize', function() {
             }
         });
 
-        it('should have correct default request options', function() {
+        it('should have correct default request options', function () {
             assert.deepEqual(telize.defaultRequestOptions, defaultRequestOptions, 'default request options should have correct initial property values');
         });
 
-        it('should not override default request options', function() {
+        it('should not override default request options', function () {
             telize.defaultRequestOptions = {
                 hostname: 'www.telize.co',
                 port: 3000,
@@ -28,14 +28,14 @@ describe('telize', function() {
         });
     });
 
-    describe('functions', function() {
-        describe('#getIP()', function() {
-            it('should respond with IP address', function(done) {
-                telize.getIP(function(error, ip) {
+    describe('functions', function () {
+        describe('#getIP()', function () {
+            it('should respond with IP address', function (done) {
+                telize.getIP(function (error, ip) {
                     if (!error) {
                         assert.notEqual(ip, undefined, 'ip should not be undefined');
                     } else {
-                        assert.throws(function() {
+                        assert.throws(function () {
                             throw error;
                         }, Error);
                     }
@@ -43,24 +43,24 @@ describe('telize', function() {
                 });
             });
 
-            it('should fail when called without arguments', function() {
-                assert.throws(function() {
+            it('should fail when called without arguments', function () {
+                assert.throws(function () {
                     telize.getIP();
                 }, Error);
             });
 
-            it('should not be overridden', function() {
-                telize.getIP = function() {
+            it('should not be overridden', function () {
+                telize.getIP = function () {
                     return '#getIP()';
                 };
 
-                assert.throws(function() {
+                assert.throws(function () {
                     assert.notEqual(telize.getIP(), '#getIP()', '#getIP() should not be overridden');
                 }, Error);
             });
         });
 
-        describe('#getGeoIP()', function() {
+        describe('#getGeoIP()', function () {
             function validateGeoIPData(data) {
                 assert.notEqual(data, undefined, 'data should not be undefined');
                 assert.notEqual(data.longitude, undefined, 'data.longitude should not be undefined');
@@ -78,12 +78,12 @@ describe('telize', function() {
                 assert.notEqual(data.country_code3, undefined, 'data.country_code3 should not be undefined');
             }
 
-            it('should respond with GeoIP data object for current IP address', function(done) {
-                telize.getGeoIP(function(error, data) {
+            it('should respond with GeoIP data object for current IP address', function (done) {
+                telize.getGeoIP(function (error, data) {
                     if (!error) {
                         validateGeoIPData(data);
                     } else {
-                        assert.throws(function() {
+                        assert.throws(function () {
                             throw error;
                         }, Error);
                     }
@@ -91,12 +91,12 @@ describe('telize', function() {
                 });
             });
 
-            it('should respond with GeoIP data object for manually set IP address', function(done) {
-                telize.getGeoIP('46.19.37.108', function(error, data) {
+            it('should respond with GeoIP data object for manually set IP address', function (done) {
+                telize.getGeoIP('46.19.37.108', function (error, data) {
                     if (!error) {
                         validateGeoIPData(data);
                     } else {
-                        assert.throws(function() {
+                        assert.throws(function () {
                             throw error;
                         }, Error);
                     }
@@ -104,12 +104,12 @@ describe('telize', function() {
                 });
             });
 
-            it('should respond with error when called with invalid IP address', function(done) {
-                telize.getGeoIP('46.19.37', function(error, data) {
+            it('should respond with error when called with invalid IP address', function (done) {
+                telize.getGeoIP('46.19.37', function (error, data) {
                     if (!error) {
                         validateGeoIPData(data);
                     } else {
-                        assert.throws(function() {
+                        assert.throws(function () {
                             throw error;
                         }, Error);
                     }
@@ -117,24 +117,24 @@ describe('telize', function() {
                 });
             });
 
-            it('should fail when called without arguments', function() {
-                assert.throws(function() {
+            it('should fail when called without arguments', function () {
+                assert.throws(function () {
                     telize.getGeoIP();
                 }, Error);
             });
 
-            it('should fail when called with IP address only', function() {
-                assert.throws(function() {
+            it('should fail when called with IP address only', function () {
+                assert.throws(function () {
                     telize.getGeoIP('46.19.37.108');
                 }, Error);
             });
 
-            it('should not be overridden', function() {
-                telize.getGeoIP = function() {
+            it('should not be overridden', function () {
+                telize.getGeoIP = function () {
                     return '#getGeoIP()';
                 };
 
-                assert.throws(function() {
+                assert.throws(function () {
                     assert.notEqual(telize.getGeoIP(), '#getGeoIP()', '#getGeoIP() should not be overridden');
                 }, Error);
             });
